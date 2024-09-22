2248765626fce1a419ccc8d50f91786e08842102
Parth Vaidya <zeusparth99@gmail.com>
2024-09-22 11:07:59 +0530

    Added Main Function for Player Input

---
+++ Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string player_name;
+    cout << "Enter your name: ";
+    cin >> player_name;
+    cout << "Great Start " << player_name << " , Keep up with the Adventure champ All the Best!" << endl;
+    return 0;
+}
