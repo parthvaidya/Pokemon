6275ab6e62f6ae2003ef499932d8553d6f38f82a
Parth Vaidya <zeusparth99@gmail.com>
2024-09-24 14:38:27 +0530

    Minor Updated to Professor Oak Dialogues

--- Main.cpp
+++ Main.cpp
@@ -2,12 +2,22 @@
 using namespace std;
 
 int main() {
+
+    string player_name;
+
     //  Greet the Player
     cout << "Professor Oak: \n";
     cout << "\"Ah, Trainer! Welcome to the world of Pokémon!\"\n";
-    cout << "\"Today is a momentous day—you’ll be choosing your very first Pokémon.\"\n";
+    cout << "\"Today is a momentous day—youll be choosing your very first Pokémon.\"\n";
     cout << "\"Every great Trainer remembers this moment for the rest of their lives.\"\n";
     cout << "\"So, choose wisely, young one!\"\n\n";
+
+    cout << "Professor Oak: First, tell me, whats your name?\n";
+    cin >> player_name;
+
+    cout << "Professor Oak: Ah, " << player_name << "! What a fantastic name!\n";
+    cout << "Professor Oak: You must be eager to start your adventure. But "
+                "first, youll need a Pokemon of your own!\n";
 
     //  Display the Pokémon Choices
     cout << "Choose your Pokémon:\n";
@@ -33,8 +43,8 @@ int main() {
     }
 
     cout << "\nProfessor Oak: \n";
-    cout << "\"Ah, an excellent choice! But beware, Trainer, this is only the beginning.\"\n";
-    cout << "\"Your journey is about to unfold. Now let’s see if you’ve got what it takes to keep going!\"\n";
+    cout << "\"Ah, an excellent choice! But beware " <<player_name << ", this is only the beginning.\"\n";
+    cout << "\"Your journey is about to unfold. Now lets see if you have got what it takes to keep going!\"\n";
     cout << "\"Good luck, and remember… Choose wisely!\"\n";
 
     return 0;
